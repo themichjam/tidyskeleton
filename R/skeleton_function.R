98ad01d3f390d0d21e6bd8e8869370558ddff4ea
Michelle Jamieson <38766192+themichjam@users.noreply.github.com>
2023-08-14 17:24:28 +0100

    now 'project'

--- R/skeleton_function.R
+++ R/skeleton_function.R
@@ -8,28 +8,28 @@
 #' skeleton()
 
 skeleton <- function(dircreate){
-  ifelse(!dir.exists("project_test"),
-         dir.create("project_test"), "Folder exists already")
-  ifelse(!dir.exists("project_test/data"),
-         dir.create("project_test/data"), "Folder exists already")
-  ifelse(!dir.exists("project_test/data/raw"),
-         dir.create("project_test/data/raw"), "Folder exists already")
-  ifelse(!dir.exists("project_test/data/processed"),
-         dir.create("project_test/data/processed"), "Folder exists already")
-  ifelse(!dir.exists("project_test/data/metadata"),
-         dir.create("project_test/data/metadata"), "Folder exists already")
-  ifelse(!dir.exists("project_test/scripts"),
-         dir.create("project_test/scripts"), "Folder exists already")
-  ifelse(!dir.exists("project_test/rmd"),
-         dir.create("project_test/rmd"), "Folder exists already")
-  ifelse(!dir.exists("project_test/docs"),
-         dir.create("project_test/docs"), "Folder exists already")
-  ifelse(!dir.exists("project_test/output"),
-         dir.create("project_test/output"), "Folder exists already")
-  ifelse(!dir.exists("project_test/R"),
-         dir.create("project_test/R"), "Folder exists already")
-  ifelse(!dir.exists("project_test/figs"),
-         dir.create("project_test/figs"), "Folder exists already")
-  ifelse(!dir.exists("project_test/imgs"),
-         dir.create("project_test/imgs"), "Folder exists already")
+  ifelse(!dir.exists("project"),
+         dir.create("project"), "Folder exists already")
+  ifelse(!dir.exists("project/data"),
+         dir.create("project/data"), "Folder exists already")
+  ifelse(!dir.exists("project/data/raw"),
+         dir.create("project/data/raw"), "Folder exists already")
+  ifelse(!dir.exists("project/data/processed"),
+         dir.create("project/data/processed"), "Folder exists already")
+  ifelse(!dir.exists("project/data/metadata"),
+         dir.create("project/data/metadata"), "Folder exists already")
+  ifelse(!dir.exists("project/scripts"),
+         dir.create("project/scripts"), "Folder exists already")
+  ifelse(!dir.exists("project/rmd"),
+         dir.create("project/rmd"), "Folder exists already")
+  ifelse(!dir.exists("projectt/docs"),
+         dir.create("project/docs"), "Folder exists already")
+  ifelse(!dir.exists("project/output"),
+         dir.create("project/output"), "Folder exists already")
+  ifelse(!dir.exists("project/R"),
+         dir.create("project/R"), "Folder exists already")
+  ifelse(!dir.exists("project/figs"),
+         dir.create("project/figs"), "Folder exists already")
+  ifelse(!dir.exists("project/imgs"),
+         dir.create("project/imgs"), "Folder exists already")
 }
